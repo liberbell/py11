2b0bafcbd321ce44bbebbb818db6476747d04c0e
Liberbell <liberbell@gmail.com>
2019-12-28 20:59:53 +0900

    functions3

--- functions.py
+++ functions.py
@@ -1,4 +1,8 @@
 def say_hello(name):
     print(f"Hello {name}")
 
+def describe_yourself(name, age):
+    print(f"My name is {name}. My age is {age}.")
+
 say_hello("bob")
+describe_yourself("Jack", "15")
