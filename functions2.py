a740c338cb751164551d0e0a90ebf8639ccfdfe6
Liberbell <liberbell@gmail.com>
2019-12-28 21:05:05 +0900

    functions2-4

--- functions2.py
+++ functions2.py
@@ -1,4 +1,7 @@
 def pizza_name(toppings):
-    print(f"{toppings.title()} Pizza")
+    # print(f"{toppings.title()} Pizza")
+    return  f"{toppings.title()} Pizza"
 
-pizza_name("pepperoni")
+# pizza_name("pepperoni")
+pizza = pizza_name("cheese")
+print(pizza)
