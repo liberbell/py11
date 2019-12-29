2ff99c77543b4d8fadc16ddefec0474188b6cdad
Liberbell <liberbell@gmail.com>
2019-12-29 10:58:27 +0900

    loops4

--- loops.py
+++ loops.py
@@ -4,3 +4,10 @@ print(pizza_toppings)
 for topping in pizza_toppings:
     message = f"I would like {topping} on my pizza"
     print(message)
+
+my_numbers = [1, 2, 3]
+my_square_numbers = []
+for number in my_numbers:
+    my_square_numbers.append(number**2)
+
+print(my_square_numbers)
