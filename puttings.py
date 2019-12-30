bf1a2a19be7ea36d40ab62a9db952218af4c0da4
Liberbell <liberbell@gmail.com>
2019-12-30 13:43:23 +0900

    puttings7

--- puttings.py
+++ puttings.py
@@ -3,9 +3,10 @@ pizza_toppings = ["cheese", "pepperoni"]
 
 def format_topping(topping):
     if topping == "cheese":
-        return f"{topping.title()} (free)"
+        return f"{topping.title()} (Free)"
     else:
         return f"{topping.title()} ($1 Extra)"
 
 # format_topping(pizza_toppings)
 print(format_topping("cheese") == "Cheese (Free)")
+print(format_topping("pepperoni") == "Pepperoni ($1 Extra)")
