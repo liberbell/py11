9f80e484074fff448bfb24e2a73b205d038eff3f
Liberbell <liberbell@gmail.com>
2019-12-31 16:13:11 +0900

    lines5

--- lines.py
+++ lines.py
@@ -5,5 +5,9 @@ screen.bgcolor("blue")
 screen.title("Drawing Lines Practice")
 
 my_turtle = turtle.Turtle()
-my_turtle.shape()
+my_turtle.pensize(5)
+my_turtle.shape("circle")
+my_turtle.forward(100)
+
+my_turtle.left(45)
 my_turtle.forward(100)
