73db81efb35fc438a3cd5542a40bab2ca143fbc2
Liberbell <liberbell@gmail.com>
2020-01-02 18:10:22 +0900

    pizzas2

---
+++ pizzas.py
@@ -0,0 +1,22 @@
+import turtle
+
+BACKGROUND_Color = "#9EC388"
+CRUST_Color = "#ECA84F"
+SAUCE_Color = "#AD0509"
+CHEESE_Color = "#FBC70F"
+
+PEPPERONI_Location = [
+    [-70, 105],
+    [-85, 175],
+    [-25, 50],
+    [-15, 100],
+    [-25, 150],
+    [-30, 205],
+    [15, 50],
+    [20, 120],
+    [20, 200],
+    [60, 156],
+    [71, 215],
+    [80, 90],
+    [95, 150]
+]
