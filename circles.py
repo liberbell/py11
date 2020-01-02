b3abc99090d06c11ea1cd24cdfd4dd03320931ee
Liberbell <liberbell@gmail.com>
2020-01-02 18:01:30 +0900

    circles6

--- circles.py
+++ circles.py
@@ -7,6 +7,10 @@ screen.title("Drawing Circle Practice")
 my_turtle = turtle.Turtle()
 my_turtle.pensize(5)
 
+my_turtle.fillcolor("red")
+my_turtle.begin_fill()
+
 my_turtle.circle(100)
+my_turtle.end_fill()
 
 turtle.done()
